d60465cc4fbad93e81cf2fb677eeb1052468a246
John <johnolshea@gmail.com>
2024-01-18 20:57:18 -0800

    Advanced task

--- monty.c
+++ monty.c
@@ -143,6 +143,162 @@ void nop(stack_t **stack, unsigned int line_number)
 }
 
 /**
+ * sub - subtracts the top element from the second top element of the stack
+ * @stack: A pointer to the head of the stack
+ * @line_number: The line number in the Monty bytecode file
+ *
+ * Description: Subtracts the top element from the second top element of the stack.
+ * If the stack contains less than two elements, prints an error message and exits
+ * with EXIT_FAILURE. The result is stored in the second top element of the stack,
+ * and the top element is removed.
+ */
+void sub(stack_t **stack, unsigned int line_number)
+{
+	if (*stack == NULL || (*stack)->next == NULL)
+	{
+		fprintf(stderr, "L%d: can't sub, stack too short\n", line_number);
+		exit(EXIT_FAILURE);
+	}
+
+	(*stack)->next->n -= (*stack)->n;
+
+	pop(stack, line_number);
+}
+
+/**
+ * div_op - divides the second top element
+ * @stack: A pointer to the head of the stack
+ * @line_number: The line number in the Monty bytecode file
+ *
+ * Description: Divides the second top
+ */
+void div_op(stack_t **stack, unsigned int line_number)
+{
+	if (*stack == NULL || (*stack)->next == NULL)
+	{
+		fprintf(stderr, "L%d: can't div, stack too short\n", line_number);
+		exit(EXIT_FAILURE);
+	}
+
+	if ((*stack)->n == 0)
+	{
+		fprintf(stderr, "L%d: division by zero\n", line_number);
+		exit(EXIT_FAILURE);
+	}
+
+	(*stack)->next->n /= (*stack)->n;
+
+	pop(stack, line_number);
+}
+
+/**
+ * mod_op - computes the rest of the division of the second top element
+ *          by the top element of the stack
+ * @stack: A pointer to the head of the stack
+ * @line_number: The line number in the Monty bytecode file
+ *
+ * Description: Computes the rest of the division of the second top element
+ */
+void mod_op(stack_t **stack, unsigned int line_number)
+{
+	if (*stack == NULL || (*stack)->next == NULL)
+	{
+		fprintf(stderr, "L%d: can't mod, stack too short\n", line_number);
+		exit(EXIT_FAILURE);
+	}
+
+	if ((*stack)->n == 0)
+	{
+		fprintf(stderr, "L%d: division by zero\n", line_number);
+		exit(EXIT_FAILURE);
+	}
+
+	(*stack)->next->n %= (*stack)->n;
+
+	pop(stack, line_number);
+}
+
+/**
+ * is_comment - checks if a line is a comment
+ * @line: The line to check
+ * Return: 1 if the line is a comment, 0 otherwise
+ */
+int is_comment(const char *line)
+{
+	while (*line)
+	{
+		if (*line == '#')
+			return 1;
+		else if (*line != ' ' && *line != '\t')
+			return 0;
+		line++;
+	}
+	return (0);
+}
+
+/**
+ * pchar - prints the char at the top of the stack
+ * @stack: A pointer to the head of the stack
+ * @line_number: The line number in the Monty file
+ *
+ * Description: Prints the ASCII character at the top of the stack.
+ * If the stack is empty or the value is out of range, prints an error message.
+ */
+void pchar(stack_t **stack, unsigned int line_number)
+{
+int value;
+stack_t *temp = *stack;
+
+if (*stack == NULL)
+{
+fprintf(stderr, "L%d: can't pchar, stack empty\n", line_number);
+exit(EXIT_FAILURE);
+}
+
+
+value = temp->n;
+
+if (value < 0 || value > 127)
+{
+fprintf(stderr, "L%d: can't pchar, value out of range\n", line_number);
+exit(EXIT_FAILURE);
+}
+
+putchar(value);
+putchar('\n');
+
+*stack = temp->next;
+if (*stack)
+(*stack)->prev = NULL;
+free(temp);
+}
+
+#include <stdio.h>
+
+/**
+ * pstr - prints the string starting at the top of the stack
+ * @stack: A pointer to the head of the stack
+ * @line_number: The line number in the Monty file
+ *
+ * Description: Prints the string starting at the top of the stack.
+ */
+void pstr(stack_t **stack, unsigned int line_number)
+{
+stack_t *current = *stack;
+
+(void)line_number;
+
+while (current != NULL && current->n != 0 && (current->n >= 0 && current->n <= 127))
+{
+_putchar(current->n);
+current = current->next;
+}
+
+_putchar('\n');
+}
+
+
+/**
  * push - pushes an element onto the stack
  * @stack: A pointer to the head of the stack
  * @value_str: The string representation of the value to push
@@ -241,6 +397,9 @@ int main(int argc, char *argv[])
 
 		line_number++;
 
+		if (is_comment(line))
+			continue;
+
 		line[strcspn(line, "\n")] = '\0';
 
 		if (opcode != NULL)
@@ -282,6 +441,26 @@ int main(int argc, char *argv[])
 			else if (strcmp(opcode, "nop") == 0)
 			{
 				nop(&stack, line_number);
+			}
+			else if (strcmp(opcode, "sub") == 0)
+			{
+				sub(&stack, line_number);
+			}
+			else if (strcmp(opcode, "div") == 0)
+			{
+				div_op(&stack, line_number);
+			}
+			else if (strcmp(opcode, "mod") == 0)
+			{
+				mod_op(&stack, line_number);
+			}
+			else if (strcmp(opcode, "pchar") == 0)
+			{
+				pchar(&stack, line_number);
+			}
+			else if (strcmp(opcode, "pstr") == 0)
+			{
+				pstr(&stack, line_number);
 			}
 			else
 			{
